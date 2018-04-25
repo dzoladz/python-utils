ed623072863fe1c1bbce6670cab712dbef241e30
dzoladz <derek@derekzoladz.com>
2018-04-25 18:22:39 -0400

    add jest in description

---
+++ sql-generator.py
@@ -0,0 +1,30 @@
+# SQL GENERATOR
+# Process a .csv file to generate SQL
+#
+# When you don't have a hatchet, just a hammer and a chisel.
+#
+#!/usr/local/bin/python3
+
+import csv, argparse
+
+# Establish command line values
+parser = argparse.ArgumentParser(description='Read an CVS Input File to Generate SQL to Remove Patron Alert Message')
+parser.add_argument('--infile', '-i', type=str, required=True, help='Path to the .csv file')
+parser.add_argument("--outfile", "-o", type=str, required=True, help='Path for the output file of generated SQL')
+args = parser.parse_args()
+
+# Open output file set with command line arg
+outfile = open(args.outfile, 'w')
+
+# Begin Transaction Block
+outfile.write("Begin;" + '\n')
+
+# Iterate through the .cvs input file set with command line arg, write SQL
+with open(args.infile, newline='') as inputfile:
+    rows = csv.reader(inputfile)
+    next(rows) # remove header row
+    for row in rows:
+        outfile.write("UPDATE asset.copy_note SET pub = 'f', title = '', value = '' WHERE id =  '" + row[0].strip() + "';" + '\n')
+
+## Close the file
+outfile.close()
